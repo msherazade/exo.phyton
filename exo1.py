9946e1cf7623c09d38c795dcdfdba338c2e8a8a9
John Doe <johndoe@example.com>
2023-11-21 09:16:47 +0100

    1commit

---
+++ exo1.py
@@ -0,0 +1,31 @@
+var1 = 1
+var2 = 3
+moi = "bonjour"
+mot = "ronjonb"
+
+var1 + var2
+moi + mot
+
+for i in [0, 1, 2, 3]:
+    print("i a pour valeur", i)
+
+for i in range(4):
+    print("i a pour valeur", i)
+
+
+    prompt = "bonjour"
+    array = list(prompt)
+    resulta
+
+
+
+ 
+
+
+
+
+
+   
+
+
+
---
+++ exo2.py
@@ -0,0 +1,16 @@
+array= [1, 2, 3, 4] 
+result=0
+print (array)
+
+count = len(array)
+for i in range(count):
+    result =result + count+1
+    print(result)
+
+
+   
+
+   
+
+
+ 
