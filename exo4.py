de1d9bff78c5419e81f1244455e6ab88189c2e42
John Doe <johndoe@example.com>
2023-11-21 16:39:59 +0100

    correction

--- exo4.py
+++ exo4.py
@@ -9,3 +9,20 @@ for i in range(1, nbr+1):
     fact = fact * i
 print (fact)
 
+
+def factorielle (num):
+        result = 1
+        i = 0
+        for i in range(num):
+              result = result *(num-1)
+              i=i+1
+print(result) 
+
+
+#autre facon de faire par tom 
+entree = 5
+for var in range(entree - 1):
+    var = var + 1             # mettre un print ici pour debugger et voir ce qui se passe
+    entree = var * entree
+
+      
