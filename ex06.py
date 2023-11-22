88527ded8a4270912cddf0e62eb53102f657163f
sherazade mok <s.mokdad@it-students.fr>
2023-11-22 16:43:51 +0100

    mofif

--- ex06.py
+++ ex06.py
@@ -8,10 +8,40 @@ for i in phrase:
 print(compteur)
 
 #facon split
-entree ="le test logiciel est logiciel"
+entree ="p l m"
 nombre_de_mot = 0
 for mot in entree.split():
     nombre_de_mot = nombre_de_mot + 1
 print(nombre_de_mot)    
+
+#autre facon
+chaine = "moi loo nous vous a demain"
+def compter_mots(chaine):
+    return len(chaine.split())
+print(len(chaine.split()))
+
+#autre methode par tom methode foreach
+prompt = "le test"
+array = []
+space = False
+var = ""
+for i in prompt:
+    if i == "":
+        space = True
+    else :#concatenation
+         space = False  
+    if space :
+        array.append(var) #ajouter un element au tableau
+        var = ""       
+    else :
+        var = var + i    
+if i != "":
+     array.append(var)     
+
+
+result = 0
+for i in array :     
+    result = result + 1
+print(result)    
 
 
